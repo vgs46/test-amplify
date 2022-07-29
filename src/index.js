0b2d0e5d36c90ff3aa77f844d8589551fe43806e
Ganesh Shankar <ganesh.voona@avaamo.com>
2022-07-29 15:24:04 +0530

    changed index

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import config from './aws-exports';
 import Amplify from 'aws-amplify';
+
+
 import "@aws-amplify/ui-react/styles.css";
 import {AmplifyProvider} from "@aws-amplify/ui-react";
 
-import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+
+Amplify.configure(config);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
